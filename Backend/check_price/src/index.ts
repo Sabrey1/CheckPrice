de9611ffaff00cfd4bb0f72011b08c37f15145c0
Sabrey1 <limsabriey94@gmail.com>
2026-08-08 16:27:39 +0700

    update

--- Backend/check_price/src/index.ts
+++ Backend/check_price/src/index.ts
@@ -19,7 +19,8 @@ app.use(
     origin: [
       'http://localhost:5173',
       'http://127.0.0.1:5173',
-      'http://localhost:3000'
+      'http://localhost:3000',
+      'https://checkprice.sabrey.xyz'
     ],
   })
 )
